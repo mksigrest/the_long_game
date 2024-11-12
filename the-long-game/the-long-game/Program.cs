4242e4fc133a26a4754a5759b63dc2b1adb58052
mksigrest <mksigrest@gmail.com>
2024-11-12 08:37:25 -0500

    intitial program upload
    created score that incriments when key pressed

---
+++ the-long-game/the-long-game/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
+namespace the_long_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            name = Console.ReadLine();
+            int score = 0;
+            ConsoleKeyInfo cki;
+            cki = Console.ReadKey();
+            
+
+            if (cki.Equals(cki))
+            {
+                if (cki.Key == ConsoleKey.Enter)
+                {
+
+                }
+
+                else
+                {
+                    score++;
+                }
+            }
+        }
+    }
+}
